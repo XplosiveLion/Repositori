51b0e39d012ae139a7f0f78aea63aa60b8a5736b
XplosiveLion <137921084+XplosiveLion@users.noreply.github.com>
2026-04-16 17:02:33 -0600

    nuevos ejercicios

---
+++ JS Error Handling/4-Custom Error on a negative number/custom.js
@@ -0,0 +1,9 @@
+function validate_Positive_Number(n) {
+  if (n < 0) {
+    throw new Error('Error: Negative numbers are not allowed.');
+  }
+  return n;
+}
+
+console.log(validate_Positive_Number(3));
+console.log(validate_Positive_Number(-5));
